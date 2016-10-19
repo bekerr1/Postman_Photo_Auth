c477f5a5a07049e8d9b2234235b33b346377a158
Brendan Kerr <brendkerr5@gmail.com>
2016-10-19 17:11:24 -0400

    Initial Commit

---
+++ iOSResearchPosition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSResearchPosition
+//
+//  Created by brendan kerr on 10/19/16.
+//  Copyright © 2016 b3k3r. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
